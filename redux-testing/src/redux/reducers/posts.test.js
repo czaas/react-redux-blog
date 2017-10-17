2e25558e2d31b5cd8e0e3c7ff4c7e7ba60b9dc5f
Cameron Zaas <cameron.zaas@mindbodyonline.com>
2017-10-17 12:43:47 -0700

    Added tests for getting and receiving post comments

--- redux-testing/src/redux/reducers/posts.test.js
+++ redux-testing/src/redux/reducers/posts.test.js
@@ -24,6 +24,7 @@ const somePosts = [
         "deleted": false
     }
 ];
+const someComments = [{ id: '123', comment: 'Yes' }, { id: '321', comment: 'No' }];
 
 describe('posts reducer', () => {
 	it('should return default state', () => {
@@ -64,6 +65,41 @@ describe('posts reducer', () => {
 		).toEqual({
 			fetching: false,
 			posts: somePosts
+		});
+	});
+
+	it('should request posts and trigger the fetching comments to be true and reset old comments', () => {
+		expect(
+			postsReducer(
+				{
+					currentPostCommentsId: 'fakeid-124',
+					currentPostComments: someComments,
+				}, {
+					type: actions.GET_POST_COMMENTS,
+					id: 'fakeid-123',
+				}
+			)
+		).toEqual({
+			currentPostComments: [],
+			currentPostCommentsId: 'fakeid-123',
+			fetchingComments: true,
+		});
+	});
+
+	it('should receive the post comments and update state and set fetching to false', () => {
+		expect(
+			postsReducer(
+				{
+					fetchingComments: true
+				},
+				{
+					type: actions.RECEIVE_POST_COMMENTS,
+					comments: someComments,
+				}
+			)
+		).toEqual({
+			fetchingComments: false,
+			currentPostComments: someComments,
 		});
 	});
 });
