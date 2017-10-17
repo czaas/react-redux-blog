f587474c7dac348ed31ac45be11379b2a3fd274e
Cameron Zaas <cameron.zaas@mindbodyonline.com>
2017-10-17 11:18:08 -0700

    Added comments to post reducers

--- redux-testing/src/redux/reducers/posts.js
+++ redux-testing/src/redux/reducers/posts.js
@@ -2,12 +2,15 @@ import { unionBy } from 'lodash';
 import {
 	REQUEST_POSTS,
 	RECEIVE_POSTS,
+	GET_POST_COMMENTS,
+	RECEIVE_POST_COMMENTS,
 } from '../actions';
 
 
 export const initialPostState = {
 	fetching: false,
 	fetchingComments: false,
+	currentPostCommentsId: '',
 	currentPostComments: [],
 	posts: [],
 };
@@ -15,18 +18,32 @@ function posts(state = initialPostState, action) {
 	if (action && action.type) {
 		switch(action.type) {
 			case REQUEST_POSTS:
-				return {
+				return Object.assign({}, state, {
 					fetching: true,
-				};
+				});
 			case RECEIVE_POSTS:
 				let statePosts = state.posts ? state.posts : [];
 
-			return {
-				fetching: false,
-				posts: unionBy(statePosts, action.posts, 'id'),
-			}
-		default:
-			return state;
+				return Object.assign({}, state, {
+					fetching: false,
+					posts: unionBy(statePosts, action.posts, 'id'),
+				});
+			case GET_POST_COMMENTS:
+				return Object.assign({}, state, {
+					fetchingComments: true,
+					currentPostCommentsId: action.id,
+					currentPostComments: [],
+				});
+			case RECEIVE_POST_COMMENTS:
+				return Object.assign({}, state, {
+					fetchingComments: false,
+					currentPostComments: action.comments,
+				});
+			default:
+				return state;
+		}
+	} else {
+		return state;
 	}
 }
 
